7faa94d0c5fb4952ee2ea31661a2bedfeb1f9757
DarlanCosta <darlandsv@gmail.com>
2019-11-12 16:33:12 -0300

    2019-11-12 finish

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,22 +4,16 @@ import Imagem from '../assets/perfil.png';
 
 function Header() {
   return(
-  <header className="site-header">
-
-    <div className="title">
-      <h1>facebook.</h1>
-    </div>
-    
-    <div className="logo">
-      <h5>Meu perfil</h5>
-      <img src={Imagem} />
-    </div>
-    
-
-  </header>
-  
-
-  
+    <header className="site-header">
+      <div className="title">
+        <h1>facebook.</h1>
+      </div>
+      
+      <div className="logo">
+        <h5>Meu perfil</h5>
+        <img src={Imagem} />
+      </div>
+    </header> 
   )
 }
 
